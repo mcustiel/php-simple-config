a578addfbf34d02f0645e77d6c35aadd7bc8f373
Mariano Custiel <jmcustiel@gmail.com>
2014-06-27 00:15:10 -0300

    Fixed exception

--- src/mcustiel/config/exception/ConfigException.php
+++ src/mcustiel/config/exception/ConfigException.php
@@ -1,16 +1,16 @@
 <?php
 namespace mcustiel\config\exception;
 
-class ConfigException extends Exception
+class ConfigException extends \Exception
 {
     private static $exceptions = array(
         0 => "Unknown exception",
-        1 => "The selected key does not exist"
+        "The selected key does not exist"
     );
     const EXCEPTION_UNKNOWN = 0;
     const EXCEPTION_KEY_DOES_NOT_EXIST = 1;
 
-    public function __construct($exceptionId, Exception $previous = null)
+    public function __construct($exceptionId, \Exception $previous = null)
     {
         if (isset(self::$exceptions[$exceptionId])) {
             parent::__construct(self::$exceptions[$exceptionId], $exceptionId, $previous);
