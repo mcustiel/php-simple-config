07145eff7c4971a946f78f8085ad8e63d55d7f66
Mariano Custiel <mcustiel@gmail.com>
2014-11-27 21:26:44 -0200

    Added copyright notes in every file

--- src/Mcustiel/Config/Drivers/Writer/php/Writer.php
+++ src/Mcustiel/Config/Drivers/Writer/php/Writer.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * This file is part of php-simple-config.
+ * 
+ * php-simple-config is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * php-simple-config is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with php-simple-config.  If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace Mcustiel\Config\Drivers\Writer\php;
 
 use Mcustiel\Config\Config;
@@ -14,6 +30,22 @@ class Writer
 
     public function write($filename)
     {
-        file_put_contents($filename, '<?php' . PHP_EOL . 'return ' . var_export($this->config, true) . ';' . PHP_EOL);
+        file_put_contents($filename, '<?php
+/**
+ * This file is part of php-simple-config.
+ * 
+ * php-simple-config is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * php-simple-config is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with php-simple-config.  If not, see <http://www.gnu.org/licenses/>.
+ */' . PHP_EOL . 'return ' . var_export($this->config, true) . ';' . PHP_EOL);
     }
 }
